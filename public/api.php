cac56a442970324979de6d0b41e881d40488a61e
JonatanV <jonatan.vannberg@elev.ga.ntig.se>
2020-05-20 14:30:14 +0200

    hallå fungerardet här ens

--- public/api.php
+++ public/api.php
@@ -5,10 +5,22 @@
     $dbInstance = DbConnection::getInstance();
     $dbh = $dbInstance->getConnection();
 
-    $sth = $dbh->prepare('SELECT * FROM likes');
-    $sth->execute();
-    $result = $sth->fetchAll(PDO::FETCH_ASSOC);
-    header('Content-Type: application/json');
-    echo json_encode($result);
+    if ($_GET['all']) {
+        $sth = $dbh->prepare('SELECT * FROM likes');
+        $sth->execute();
+        $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+        echo json_encode($result);
+    } else if (isset($_GET['id'])) {
+        echo json_encode('route for fetching resource by id');
+    }
+
+    if(isset($_GET['id'])) {
+        $id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
+
+        echo json_encode($id);
+    } 
+
+
     
 ?>
